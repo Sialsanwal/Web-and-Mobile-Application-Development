1faeba6621079b177750735fc30fe105a8242c07
Sanwal Khan <sialsanwal885@gmail.com>
2023-01-27 09:35:07 -0700

    update

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const urdu=56;
 var name="sanwal";
 eng=46;
 eng=46+2;
+f_name='Sanwal'
+l_name='khan'
 
 var mul_Line_string=` sanwal
 khan 
@@ -14,3 +16,4 @@ laptop`
 // console.log(urdu);
 console.log(name);
 console.log(mul_Line_string);
+console.log(f_name+" "+l_name);
