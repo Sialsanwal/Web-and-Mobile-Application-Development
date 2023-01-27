5a31142db22fe39e861e951eeb9cfe9b610f9c50
Sanwal Khan <sialsanwal885@gmail.com>
2023-01-27 10:07:28 -0700

    update

--- js/script.js
+++ js/script.js
@@ -6,6 +6,17 @@ eng=46;
 eng=46+2;
 f_name='Sanwal'
 l_name='khan'
+var win= true;
+var names=['Ali',"Ahmad","Anaya"];
+var marks=[12,54,65];
+var stdName=[];
+stdName[0]="Sanwal";
+stdName[1]="ahmad";
+stdName[2]="aadil";
+ console.log(stdName);
+ var names2= new Array("Sanwal","ahmad","Aadil");
+ console.log(names2);
+
 
 var mul_Line_string=` sanwal
 khan 
@@ -17,3 +28,22 @@ laptop`
 console.log(name);
 console.log(mul_Line_string);
 console.log(f_name+" "+l_name);
+console.log(win);
+console.log(names[2]);
+console.log(marks[2]);
+console.log(names);
+names.splice(0,1);
+console.log(names);
+names.push("ikram");
+console.log(names);
+names.push("zeeshan");
+names.push("bilal");
+names.push("hamza");
+console.log(names);
+names.sort();
+console.log(names);
+names.reverse();
+console.log(names);
+
+
+
