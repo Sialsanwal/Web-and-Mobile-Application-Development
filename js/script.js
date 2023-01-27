c806386774f7d0b0fd202795d738cb5e3a27c903
Sanwal Khan <sialsanwal885@gmail.com>
2023-01-26 20:03:45 -0700

    update

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 var eng=32;
-console.log(eng)
+let chem=75;
+const urdu=56;
+var name="sanwal";
+eng=46;
+eng=46+2;
+
+var mul_Line_string=` sanwal
+khan 
+hp
+laptop`
+// console.log(chem);
+// console.log(eng);
+// console.log(urdu);
+console.log(name);
+console.log(mul_Line_string);
